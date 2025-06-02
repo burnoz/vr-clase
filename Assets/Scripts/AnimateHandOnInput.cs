6cce0e3d48978817949dee763cd0938b3fb23232
Bruno Zabala <brunozabalap@gmail.com>
2025-06-02 14:52:07 -0600

    VR Game

---
+++ Assets/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
+
+    public void Update()
+    {
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", triggerValue);
+    }
+}
---
+++ Assets/Scripts/FireBulletOnActivate.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBulletOnActivate : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 20;
+
+    void Start()
+    {
+        UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable
+            grabbable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet);
+
+        spawnedBullet.transform.position = spawnPoint.position;
+
+        spawnedBullet.GetComponent<Rigidbody>().linearVelocity = spawnPoint.forward * fireSpeed;
+
+        Destroy(spawnedBullet, 5);
+    }
+}
